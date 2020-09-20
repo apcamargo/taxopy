2c94aed3a57f38190ca181de89df0601f4c4edfc
Antonio Camargo <antoniop.camargo@gmail.com>
2020-09-20 11:40:36 -0300

    Allow `fraction` to be less than 0.5

--- taxopy/utilities.py
+++ taxopy/utilities.py
@@ -76,8 +76,7 @@ def find_majority_vote(
         A TaxDb object.
     fraction: float, default 0.5
         The returned taxon will be shared by more than `fraction` of the input
-        taxa lineages. This value must be equal to or greater than 0.5 and less
-        than 1.
+        taxa lineages. This value must be greater than 0.0 and less than 1.
     weights: list, optional
         A list of weights associated with the taxa lineages in `taxon_list`.
         These values are used to weight the votes of their associated lineages.
@@ -86,17 +85,18 @@ def find_majority_vote(
     -------
     Taxon
         The Taxon object of the most specific taxon that is shared by more than
-        half of the input lineages.
+        a specified fraction of the input lineages.
 
     Raises
     ------
     MajorityVoteError
         If the input taxon list has less than two Taxon objects or if the
-        `fraction` parameter is less than 0.5 or equal to or greater than 1.
+        `fraction` parameter is less than or equal to 0.0 or greater than or
+        equal to 1.
     """
-    if fraction < 0.5 or fraction >= 1:
+    if fraction <= 0.0 or fraction >= 1:
         raise MajorityVoteError(
-            "The `fraction` parameter must be equal to or greater than 0.5 and less than 1."
+            "The `fraction` parameter must be greater than 0.0 and less than 1."
         )
     if len(taxon_list) < 2:
         raise MajorityVoteError(
@@ -117,13 +117,25 @@ def find_majority_vote(
                 majority_taxon[taxon] += weight
             majority_taxon = sorted(
                 majority_taxon.items(), key=lambda x: x[1], reverse=True
-            )[0]
-            if majority_taxon[0] and majority_taxon[1] > total_weight * fraction:
-                return Taxon(majority_taxon[0], taxdb)
+            )
+            if majority_taxon[0][0] and majority_taxon[0][1] > total_weight * fraction:
+                if (
+                    len(majority_taxon) > 1
+                    and majority_taxon[0][1] > majority_taxon[1][1]
+                ):
+                    return Taxon(majority_taxon[0][0], taxdb)
+                elif len(majority_taxon) == 1:
+                    return Taxon(majority_taxon[0][0], taxdb)
     else:
         n_taxa = len(taxon_list)
         for taxonomic_level in reversed(zipped_taxid_lineage):
-            majority_taxon = Counter(taxonomic_level).most_common()[0]
-            if majority_taxon[0] and majority_taxon[1] > n_taxa * fraction:
-                return Taxon(majority_taxon[0], taxdb)
+            majority_taxon = Counter(taxonomic_level).most_common()
+            if majority_taxon[0][0] and majority_taxon[0][1] > n_taxa * fraction:
+                if (
+                    len(majority_taxon) > 1
+                    and majority_taxon[0][1] > majority_taxon[1][1]
+                ):
+                    return Taxon(majority_taxon[0][0], taxdb)
+                elif len(majority_taxon) == 1:
+                    return Taxon(majority_taxon[0][0], taxdb)
     return Taxon("1", taxdb)
